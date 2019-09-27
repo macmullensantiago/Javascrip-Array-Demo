2cc71f0c80d526a35f0b02060e0aaed6b09cfc7d
santoshyadav198613 <santosh.yadav198613@gmail.com>
2019-09-27 15:50:57 +0530

    concat method

--- index.ts
+++ index.ts
@@ -1,9 +1,16 @@
 // Import stylesheets
-import './style.css';
+import "./style.css";
 
+let cat = [];
 // Write TypeScript code!
-const appDiv: HTMLElement = document.getElementById('app');
+const appDiv: HTMLElement = document.getElementById("app");
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
 
-const categories: HTMLElement = document.getElementById('category');
+const categories: HTMLElement = document.getElementById("category");
 
+const category: HTMLElement = document.getElementById("categories");
+
+categories.addEventListener(
+  "change",
+  () => (category.innerHTML = cat.concat(categories.value).toString())
+);
